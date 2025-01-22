0cfadacb73eee76ac7eaac959a750db12d4c0f1b
Ethan Francis <ethanfrancis577@gmail.com>
2025-01-21 19:03:15 -0500

    chore: added comments

--- src/main/cpp/subsystems/ElevatorSubsystem.cpp
+++ src/main/cpp/subsystems/ElevatorSubsystem.cpp
@@ -55,8 +55,8 @@ void ElevatorSubsystem::Periodic() {
                 _elevator_pid_controller.Reset();
                 _elevator_state = ready;
             }
-            
             break;
+            // the 'home' state is the state that the elevator is in when it is at the home position.
         case ready:
             current_state = _elevator_trapezoid.Calculate(_trapezoid_timer.Get(), _initial_state, _target_state);
             feed_forward_output = _elevator_feed_forward.Calculate(_GetElevatorVelocity(), meters_per_second_t{current_state.velocity});
@@ -64,15 +64,18 @@ void ElevatorSubsystem::Periodic() {
             _primary_motor.SetVoltage(feed_forward_output+pid_output);
             
             break;
+            // the 'ready' state is the state that the elevator is in when it is not at the home position.
         case test:
             frc::SmartDashboard::PutNumber("Elevator Height (in)", _GetElevatorHeight().value());
             frc::SmartDashboard::PutNumber("Elevator Stall", _GetStallPercentage());
             frc::SmartDashboard::PutBoolean("Home Sensor", _HomeSensor());
             break;
+        // the 'test' state is for debugging.
         default:
             _elevator_state = home;
             break;
     }
+    // up above is a big switch statement that contains the different states of the elevator.
 }
 
 void ElevatorSubsystem::SetHeight(inch_t height) {
@@ -84,16 +87,19 @@ void ElevatorSubsystem::SetHeight(inch_t height) {
         _trapezoid_timer.Reset();
     }
 }
+// this function sets the height of the elevator
 
 bool ElevatorSubsystem::AtTargetHeight() {
     return math::abs(_target_state.position - _GetElevatorHeight()) < POSITION_TOLERANCE;
 }
+// this function checks if the elevator is at the target height
 
 void ElevatorSubsystem::SetPower(double power) {
     if (_elevator_state == test) {
         _primary_motor.Set(power);
     }
 }
+// this function sets the power of the elevator
 
 void ElevatorSubsystem::SetTestMode(bool test_mode) {
     if (test_mode) {
@@ -103,23 +109,29 @@ void ElevatorSubsystem::SetTestMode(bool test_mode) {
         _elevator_pid_controller.Reset();
     }
 }
+// this function sets the elevator to test mode, or sets it to home mode if it is in test mode.
 
 bool ElevatorSubsystem::_HomeSensor() {
     return !_home_sensor.Get();
 }
+// this function checks if the home sensor is triggered
 
 bool ElevatorSubsystem::_GetStalled() {
     return _GetStallPercentage() > STALL_LIMIT;
 }
+// this function checks if the elevator is stalled
 
 double ElevatorSubsystem::_GetStallPercentage() {
     return (_primary_motor.GetSupplyCurrent().GetValue()/(_primary_motor.GetMotorStallCurrent().GetValue()*abs(_primary_motor.Get())));
 }
+// this function gets the percentage of the stall current of the motor, to check if the motor is stalled
 
 inch_t ElevatorSubsystem::_GetElevatorHeight() {
     return _primary_motor.GetPosition().GetValue() * ELEVATOR_RATIO;
 }
+// this function gets the height of the elevator
 
 feet_per_second_t ElevatorSubsystem::_GetElevatorVelocity() {
     return _primary_motor.GetVelocity().GetValue() * ELEVATOR_RATIO;
 }
+// this function gets the velocity of the elevator
