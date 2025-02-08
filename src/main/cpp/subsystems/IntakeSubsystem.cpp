ca4961950d0be90e437cd0db45b0e40cf5425df1
Nithishsaran <Nithishsaran.K.S@gmail.com>
2025-02-08 15:06:52 -0500

    feat: fixed merging

--- src/main/cpp/subsystems/IntakeSubsystem.cpp
+++ src/main/cpp/subsystems/IntakeSubsystem.cpp
@@ -3,14 +3,12 @@
 
 IntakeSubsystem::IntakeSubsystem(
     int _motor_can_id,
-<<<<<<< HEAD
     int _algae_top_sensor_di_ch,
     int _algae_bottom_sensor_di_ch,
     int _coral_high_sensor_di_ch,
     int _coral_low_sensor_di_ch
     ) : 
         _intake_motor{_motor_can_id},
-<<<<<<< HEAD
         _algae_top_sensor{_algae_top_sensor_di_ch},
         _algae_bottom_sensor{_algae_bottom_sensor_di_ch},
         _coral_high_sensor{_coral_high_sensor_di_ch},
@@ -22,15 +20,7 @@ IntakeSubsystem::IntakeSubsystem(
 void IntakeSubsystem::Periodic() {
 
 }
-
-void IntakeSubsystem::SetAlgaePower(double power) {
-    _intake_motor.Set(power);
-}
-
-void IntakeSubsystem::SetCoralPower(double power) {
-=======
 void IntakeSubsystem::SetPower(double power) {
->>>>>>> 4938b3ac8a7e0d7bbf8cc16cad6331d31a556da5
     _intake_motor.Set(power);
 }
 
