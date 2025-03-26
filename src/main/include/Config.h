bbecd2ccd2d1f0b1bdcc2f2e9db374e686c02e18
Arav Bhagat <arav.bhagat@gmail.com>
2025-03-26 18:49:00 -0400

    fixed overrun error

--- src/main/include/Config.h
+++ src/main/include/Config.h
@@ -7,9 +7,9 @@
 #define VISION_ENABLED
 
 // Subsystems
-#define PIVOT_ENABLED
-#define ELEVATOR_ENABLED
-#define INTAKE_ENABLED
+// #define PIVOT_ENABLED
+// #define ELEVATOR_ENABLED
+// #define INTAKE_ENABLED
 // #define FUNNEL_ENABLED
 #define LED_ENABLED
 
